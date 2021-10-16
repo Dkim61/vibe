a4f34a54fc30cc8362a01f872bb66c102eb29c74
dkim61 <dkim187@binghamton.edu>
2021-10-15 20:33:28 -0400

    initial commit

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Grid, Button, Typography, IconButton, Icon } from "@material-ui/core";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import { Link } from "react-router-dom";
+
+const pages = {
+    JOIN: "pages.join",
+    CREATE: "pages.create",
+}
+
+export default function Info(props) {
+    // setting up state in functional component
+    const [page, setPage] = useState(pages.JOIN);
+
+    function joinInfo() {
+        return "Join Page";
+    } 
+
+    function createInfo() {
+        return "Create Page";
+    }
+
+    useEffect(() => {
+        console.log("RAN")
+        // equivilent of component unmount
+        return () => console.log("cleanup");
+    })
+
+    return (
+        <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+            <Typography component="h4" variant="h4">
+            What is Vibez?
+            </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+            <Typography variant="body=1">
+                { page === pages.JOIN ? joinInfo() : createInfo() }
+            </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+            <IconButton onClick={() => {
+                page === pages.CREATE ? setPage(pages.JOIN) : setPage(pages.CREATE)
+            }}
+            >
+                { page === pages.CREATE ? (
+                    <NavigateBeforeIcon/> 
+                ) : (
+                    <NavigateNextIcon/> 
+                )}
+            </IconButton>
+        </Grid>
+        <Grid item xs={12} align="center">
+            <Button color="secondary" variant="contained" to="/" component={Link}>
+                Back
+            </Button>
+        </Grid>
+        </Grid>
+    );
+}
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "6fdc72ef616443cba16e7698167dd454"
+CLIENT_SECRET = "a032ef9d90aa4596aecfb3f4fddff4dc"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
